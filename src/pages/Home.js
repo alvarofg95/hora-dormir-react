5aa4007ce8c20965af526c7f56858e0926f56643
Alvaro Fernandez <alvarofg95dev@gmail.com>
2019-06-05 11:44:53 +0200

    cálculo de las horas de sueño

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,19 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: null
+      error: null,
+      result: []
     };
     this.hour = React.createRef();
     this.minutes = React.createRef();
     this.type = React.createRef();
 
     this.calculate = this.calculate.bind(this);
+    this.reload = this.reload.bind(this);
   }
 
   calculate() {
+    this.setState({ loading: true, disabled: true });
     const hour = this.hour.current.value;
     const minutes = this.minutes.current.value;
     const type = this.type.current.value;
@@ -22,11 +25,8 @@ class Home extends Component {
     if (hour !== '' && minutes !== '') {
       let formattedHour = parseInt(hour);
       const formattedMinutes = parseInt(minutes);
-      if (type === 'pm') {
-        formattedHour += 12;
-      }
-      calculate(formattedHour, formattedMinutes);
-      this.setState({ error: null });
+      const result = calculate(formattedHour, formattedMinutes, type);
+      this.setState({ error: null, loading: false, result });
     } else {
       let error = 'No has seleccionado ni las horas ni los minutos';
       if (hour === '' && minutes !== '') {
@@ -34,44 +34,76 @@ class Home extends Component {
       } else if (hour !== '' && minutes === '') {
         error = 'Debes especificar en qué minuto quieres despertar';
       }
-      this.setState({ error });
+      this.setState({ error, loading: false, disabled: false });
     }
   }
 
+  reload() {
+    this.hour.current.value = '';
+    this.minutes.current.value = '';
+    this.setState({ loading: false, disabled: false, result: [], error: null });
+  }
+
   render() {
-    const { error } = this.state;
+    const { error, result, disabled } = this.state;
     return (
       <div>
         <h1>¿Cuántas horas hay que dormir?</h1>
         <p>A qué hora me tengo que dormir si me quiero despertar a las...</p>
         <div className="timeSelector">
-          <select ref={this.hour} className="selector">
+          <select disabled={disabled} ref={this.hour} className="selector">
             <option value="">Hora</option>
             {ARRAY_HOURS.map(hour => (
-              <option value={`${hour}`}>{hour}</option>
+              <option key={hour} value={`${hour}`}>
+                {hour}
+              </option>
             ))}
           </select>
-          <select ref={this.minutes} className="selector">
+          <select disabled={disabled} ref={this.minutes} className="selector">
             <option value="">Minutos</option>
             {ARRAY_MINUTES.map(minutes => (
-              <option value={`${minutes}`}>{minutes}</option>
+              <option key={minutes} value={`${minutes}`}>
+                {minutes}
+              </option>
             ))}
           </select>
-          <select ref={this.type}>
+          <select disabled={disabled} ref={this.type}>
             <option value="am">AM</option>
             <option value="pm">PM</option>
           </select>
         </div>
-        <div className="calculateDiv">
-          <button onClick={this.calculate}>
-            <img
-              width="50"
-              src={require('../images/alarm-clock.svg')}
-              alt="¿Cuántas horas hay que dormir?"
-            />
-            <span>Calcular</span>
-          </button>
-        </div>
+        {!result.length && (
+          <div className="calculateDiv">
+            <button onClick={this.calculate}>
+              <img
+                width="40"
+                src={require('../images/alarm-clock.svg')}
+                alt="¿Cuántas horas hay que dormir?"
+              />
+              <span>Calcular</span>
+            </button>
+          </div>
+        )}
+        {result.length ? (
+          <div>
+            <p>Te deberías intentar quedar dormido a estas horas:</p>
+            <div className="resultsDiv">
+              {result.map((item, i) => (
+                <span key={i}>
+                  {item.hour}:{item.minutes} {item.type.toUpperCase()}
+                </span>
+              ))}
+            </div>
+            <button className="backButton" onClick={this.reload}>
+              <img
+                width="40"
+                src={require('../images/circular-arrow.svg')}
+                alt="¿Cuántas horas hay que dormir?"
+              />
+              <span>Volver</span>
+            </button>
+          </div>
+        ) : null}
         {error && <p>{error}</p>}
       </div>
     );
--- src/utils.js
+++ src/utils.js
@@ -15,6 +15,38 @@ export const ARRAY_MINUTES = [
   '60'
 ];
 
-export const calculate = (hour, minutes) => {
-  console.log({ hour, minutes });
+const formatMinutes = minutes => {
+  const finalMinutes = minutes >= 60 ? minutes - 60 : minutes;
+  return finalMinutes >= 10 ? `${finalMinutes}` : `0${finalMinutes}`;
+};
+
+const formatHour = (hour, minutes = 0) => {
+  const finalHour = minutes >= 60 ? hour + 1 : hour;
+  return finalHour > 12 ? finalHour - 12 : finalHour;
+};
+
+export const calculate = (hour, minutes, type) => {
+  const firstHour = {
+    hour: formatHour(hour + 3),
+    minutes: minutes,
+    type: type === 'am' ? 'pm' : 'am'
+  };
+  const minutesHalf = minutes + 30;
+  const secondHour = {
+    hour: formatHour(hour + 4, minutesHalf),
+    minutes: formatMinutes(minutesHalf),
+    type: type === 'am' ? 'pm' : 'am'
+  };
+  const thirdHour = {
+    hour: formatHour(hour + 6),
+    minutes: minutes,
+    type
+  };
+  const fourthHour = {
+    hour: formatHour(hour + 7, minutesHalf),
+    minutes: formatMinutes(minutesHalf),
+    type
+  };
+
+  return [firstHour, secondHour, thirdHour, fourthHour];
 };
