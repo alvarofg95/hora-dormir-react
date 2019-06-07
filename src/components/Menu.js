0dd2f0807b7824024032f15304cf3fbfcf493b74
Alvaro Fernandez <alvarofg95dev@gmail.com>
2019-06-07 11:43:24 +0200

    menu responsive

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Image from './Image';
 
-export default () => (
-  <header>
-    <Link to="/" title="Inicio">
-      Inicio
-    </Link>
-    <Link to="/calculadora" title="Inicio">
-      Calculadora
-    </Link>
-  </header>
-);
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobile: false,
+      open: false
+    };
+    this.updateDimensions = this.updateDimensions.bind(this);
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions() {
+    const windowWidth = window.innerWidth;
+    if (windowWidth <= 400) {
+      this.setState({ mobile: true });
+    }
+  }
+
+  openMenu() {
+    this.setState(prevState => ({ open: !prevState.open }));
+  }
+
+  render() {
+    const { mobile, open } = this.state;
+    return mobile ? (
+      <div className="divMenu">
+        <button className="btnMenu" onClick={this.openMenu}>
+          <Image className="iconMenu" src={require('../images/menu.svg')} />
+        </button>
+        {open ? (
+          <div>
+            <Link className="menuItem" to="/" title="Inicio">
+              Inicio
+            </Link>
+            <Link className="menuItem" to="/calculadora" title="Inicio">
+              Calculadora
+            </Link>
+          </div>
+        ) : null}
+      </div>
+    ) : (
+      <header>
+        <Link to="/" title="Inicio">
+          Inicio
+        </Link>
+        <Link to="/calculadora" title="Inicio">
+          Calculadora
+        </Link>
+      </header>
+    );
+  }
+}
+
+export default Menu;
